39cdbb89e57f9e8147c9a38a4071655326978e90
Henrique Fadoni <henriquefadonii@gmail.com>
2019-02-04 13:20:27 -0200

    Adding Style and Creating a Sample of Messages

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React, { Component } from 'react';
 
+import './style.css';
 import MessageList from './components/MessageList';
 import SendMessageForm from './components/SendMessageForm';
 import RoomList from './components/Roomlist';
 import NewRoomForm from './components/NewRoomForm';
 
-class App extends React.Component {
+class App extends Component {
   render() {
       return (
           <div className="app">

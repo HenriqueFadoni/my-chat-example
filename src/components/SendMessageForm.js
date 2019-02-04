9f1ec2fa4010a51fcd3068dee00b679752b128f0
Henrique Fadoni <henriquefadonii@gmail.com>
2019-02-04 14:39:32 -0200

    Creating SendMessageForm

--- src/components/SendMessageForm.js
+++ src/components/SendMessageForm.js
@@ -1,15 +1,34 @@
-import React from 'react'
+import React, { Component } from 'react'
 
-class SendMessageForm extends React.Component {
+class SendMessageForm extends Component {
+    state = {
+        message: ''
+    }
+
+    handleChange = event => {
+        const newMessage = event.target.value;
+
+        this.setState({message: newMessage});
+    }
+
+    handleSubmit = event => {
+        event.preventDefault()
+        console.log(this.state.message);
+    }
+
     render() {
         return (
-            <form className="send-message-form">
+            <form
+                onSubmit={this.handleSubmit}
+                className="send-message-form">
                 <input
-                    placeholder="SendMessageForm"
+                    onChange={this.handleChange}
+                    value={this.state.message}
+                    placeholder="Type your message and hit ENTER"
                     type="text" />
             </form>
         )
     }
 }
 
-export default SendMessageForm;
+export default SendMessageForm
